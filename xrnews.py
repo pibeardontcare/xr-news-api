5150dd1dfa324e0f27a1d669ee4c566c76b036d4
Poz Long <howtopoz@gmail.com>
2025-08-01 22:00:59 -0700

    fix server wrapper

--- xrnews.py
+++ xrnews.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta, MO
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 # === CONFIG ===
@@ -68,5 +71,5 @@ def articles():
 
 # === ENTRY POINT ===
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
